fbd92a21dcb3e95b75fe37bea9c394bedd1160e3
João Lucas Pereira Rafael <joaosombrio@rede.ulbra.br>
2022-04-04 22:05:06 -0300

    Iniciando rotas

--- alura-tracker/src/components/BarraLateral.vue
+++ alura-tracker/src/components/BarraLateral.vue
@@ -3,9 +3,27 @@
         <h1>
             <img src="../assets/logo.png" alt="">
         </h1>
-        <button class="button" @click="alterarTema">
+        <div class="has-text-centered">
+            <button class="button" @click="alterarTema">
             {{textoBotao}}
-        </button>
+            </button>
+        </div>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                        <i class="fas fa-tasks"></i>
+                        tarefas
+                    </router-link>
+                </li>
+                  <li>
+                    <router-link to="/projetos" class="link">
+                        <i class="fas fa-project-diagram"></i>
+                        projetos
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -38,17 +56,34 @@
 </script>
 
 <style scoped>
+    h1{
+        text-align: center;
+    }
+    strong{
+        color: #f95738;
+    }
     header {
-    padding: 1rem;
-    background: #0d3b66;
-    width: 100%;
-    height: 100vh;
-    text-align: center;
+        background: #0d3b66;
+        width: 100%;
+        height: 100vh;
+        padding: 2rem;
     }
     @media only screen and (max-width: 768px) {
     header {
         padding: 2.5rem;
         height: auto;
     }
+    }
+    .panel li{
+        margin: 8px 0;
+    }
+    .link{
+        color: #fff;
+    }
+    .link:hover{
+        color: #FAF0CA;
+    }
+    .link.router-link-active{
+        color: #FAF0CA;
     }
 </style>
---
+++ alura-tracker/src/views/Tarefas.vue
@@ -0,0 +1,41 @@
+<template>
+    <Formulario @aoSalvarTarefa="salvarTarefa"/>
+    <div class="lista">
+    <Caixa v-if="listaEstaVazia">
+        Você não esta muito produtivo Hoje :(
+    </Caixa>        
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Formulario from '../components/Formulario.vue'
+import Tarefa from '../components/Tarefa.vue'
+import ITarefa from '../interfaces/ITarefa'
+import Caixa from '../components/Caixa.vue'
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    Formulario,
+    Tarefa,
+    Caixa
+  },
+  data(){
+    return{
+      tarefas: [] as ITarefa[],
+    }
+  },
+  computed:{
+    listaEstaVazia():boolean{
+      return this.tarefas.length === 0
+    }
+  },
+  methods:{
+        salvarTarefa(tarefa: ITarefa){
+        this.tarefas.push(tarefa)
+        },
+    },
+});
+</script>
