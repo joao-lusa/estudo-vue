44c9702d83f3ae9096848a03122abb2783c7d60b
João Lucas Pereira Rafael <joaosombrio@rede.ulbra.br>
2022-04-08 18:42:56 -0300

    Estudo de rotas com vue

--- alura-tracker/src/components/Formulario.vue
+++ alura-tracker/src/components/Formulario.vue
@@ -1,57 +1,78 @@
 <template>
-    <div class="box formulario">
-        <div class="columns">
-            <div 
-                class="column is-8" 
-                role="form" 
-                aria-label="Formulário para criação de uma nova tarefa"
+  <div class="box formulario">
+    <div class="columns">
+      <div
+        class="column is-5"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
             >
-                <input 
-                    type="text" 
-                    class="input" 
-                    placeholder="Qual tarefa você deseja iniciar?"
-                    v-model="descricao"
-                />
-            </div>
-            <div class="column">
-                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
-            </div>
+              {{ projeto.nome }}
+            </option>
+          </select>
         </div>
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <script>
-
-import { defineComponent } from 'vue';
+import { key } from "@/store";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
 import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
-    name: "FormularioDeTarefas",
-    emits:['aoSalvarTarefa'],
-    components:{
-        Temporizador
+  name: "FormularioDeTarefas",
+  emits: ["aoSalvarTarefa"],
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: '',
+      idProjeto: ''
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido) {
+      this.$emit("aoSalvarTarefa", {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao,
+        projeto: this.projetos.find(proj => proj.id == this.idProjeto)
+      });
+      this.descricao = "";
     },
-    data(){
-        return {
-            descricao: ''
-        }
-    },
-    methods:{
-        finalizarTarefa (tempoDecorrido){
-            this.$emit('aoSalvarTarefa', {
-                duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao
-            })
-            this.descricao = ''
-        }
-    },
-})
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
+});
 </script>
 <style>
-    .formulario{
-        color: var(--texto-primario);
-        background-color: var(--bg-primario);
-    }
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
 </style>
-
