fe407b997788200d2122c7fb865f981d4abde2ea
João Lucas Pereira Rafael <joaosombrio@rede.ulbra.br>
2022-04-11 09:42:18 -0300

    removendo notificação

--- alura-tracker/src/store/index.ts
+++ alura-tracker/src/store/index.ts
@@ -1,8 +1,8 @@
-import { INotificao, TipoNotificacao } from "@/interfaces/INotificao";
+import { INotificao} from "@/interfaces/INotificao";
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 
 interface Estado{
     projetos: IProjeto[],
@@ -14,26 +14,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
     state: {
         projetos: [],
-        notificacoes: [
-            {
-                id:1,
-                texto: 'uma notificação de sucesso',
-                titulo: 'sucesso',
-                tipo: TipoNotificacao.SUCESSO
-            },
-            {
-                id:2,
-                texto: 'uma notificação de aviso',
-                titulo: 'aviso',
-                tipo: TipoNotificacao.ATENCAO
-            },
-            {
-                id:3,
-                texto: 'uma notificação de falha',
-                titulo: 'falha',
-                tipo: TipoNotificacao.FALHA
-            }
-        ]
+        notificacoes: []
     },
     mutations: {
        [ ADICIONA_PROJETO](state ,nomeDoProjeto: string) {
@@ -49,6 +30,14 @@ export const store = createStore<Estado>({
         },
         [EXCLUIR_PROJETO](state, id: string){
             state.projetos = state.projetos.filter(proj => proj.id != id)
+        },
+        [NOTIFICAR] (state, novaNotificacao: INotificao) {
+            novaNotificacao.id = new Date().getTime()
+            state.notificacoes.push(novaNotificacao)
+
+            setTimeout(()=> {
+                state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
+            }, 3000)
         }
     }
 });
