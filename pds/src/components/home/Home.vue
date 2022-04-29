9aabae404546345b1b8e8d5e35db793459cdfb1f
joao-lusa <joaosombrio@gmail.com>
2022-04-29 17:13:24 -0300

    Wireframe

---
+++ pds/src/components/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="display-4 m-5">Bem vindo...</h1>
+      <h3 class="m-5">Faça aqui o orçamento dos exames</h3>
+
+        <section class="container">
+          <table class="table">
+          
+          </table>
+        </section>
+        <div class="container" >
+            <p class="m-5">Exames escolhidos </p>
+        </div>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HomeComponet',
+
+  }
+</script>
--- pds/src/views/HomeView.vue
+++ pds/src/views/HomeView.vue
@@ -4,23 +4,36 @@
       v-model="drawer"
       app
     >
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Vue.js </v-list-item-title>
+          <v-list-item-subtitle> Lista de tarefas </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      
+
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Laboratorio</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <HomeComponet />
     </v-main>
   </v-app>
 </template>
 
 <script>
+  import HomeComponet from '../components/home/Home.vue'
   export default {
     data: () => ({ drawer: null }),
+    components:{
+      HomeComponet
+    }
   }
 </script>
